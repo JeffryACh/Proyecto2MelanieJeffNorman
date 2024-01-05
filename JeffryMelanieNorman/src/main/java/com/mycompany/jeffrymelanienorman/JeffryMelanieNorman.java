b9f63fe09f3a5e669de3596af06626b9aee6819e
Jeffry Araya Ch <jeffry2811ach@gmail.com>
2024-01-05 08:46:27 -0600

    primer commit

---
+++ JeffryMelanieNorman/src/main/java/com/mycompany/jeffrymelanienorman/JeffryMelanieNorman.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jeffrymelanienorman;
+
+/**
+ *
+ * @author XPC
+ */
+public class JeffryMelanieNorman {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
