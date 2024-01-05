5364ed337fa79e7db4fc839e7d722315ced33572
mwgh05 <melwongh@gmail.com>
2024-01-05 11:44:27 -0600

    Primeras ventanas

--- JeffryMelanieNorman/src/main/java/com/mycompany/jeffrymelanienorman/JeffryMelanieNorman.java
+++ JeffryMelanieNorman/src/main/java/com/mycompany/jeffrymelanienorman/JeffryMelanieNorman.java
@@ -8,9 +8,12 @@ package com.mycompany.jeffrymelanienorman;
  *
  * @author XPC
  */
+import ventanas.Inicio;
 public class JeffryMelanieNorman {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Inicio inicio=new Inicio();
+        inicio.setVisible(true);
     }
 }
