439b0cd577ec3e7bfa418a2f504a87ee708e65bf
Jeffry Araya Ch <jeffry2811ach@gmail.com>
2024-01-13 15:46:55 -0600

    Refactor PRSReader class to improve user interface

--- JeffryMelanieNorman/src/main/java/Componentes/ListaProcesos.java
+++ JeffryMelanieNorman/src/main/java/Componentes/ListaProcesos.java
@@ -334,7 +334,7 @@ public class ListaProcesos {
      * Metodo que se encarga de retornar los procesos con el estado de "ESPERA"
      * @return procesos - ArrayList de procesos con el estado de "ESPERA"
      */
-    public ArrayList<Proceso> sacarProcesosEnEspera(){
+    private ArrayList<Proceso> sacarProcesosEnEspera(){
         ArrayList<Proceso> procesos = new ArrayList<>();
         if (sacarDocumentosEnEspera().size() != 0) {
             procesos.addAll(sacarDocumentosEnEspera());
@@ -350,4 +350,384 @@ public class ListaProcesos {
         }
         return procesos;
     }
+
+    /**
+     * Metodo que se encarga de retornar los procesos con el estado de "FINALIZADO"
+     * @return procesos - ArrayList de procesos con el estado de "FINALIZADO"
+     */
+    private ArrayList<Proceso> sacarProcesosFinalizados(){
+        ArrayList<Proceso> procesos = new ArrayList<>();
+        if (sacarDocumentosFinalizados().size() != 0) {
+            procesos.addAll(sacarDocumentosFinalizados());
+        }
+        if (sacarMultimediaFinalizados().size() != 0) {
+            procesos.addAll(sacarMultimediaFinalizados());
+        }
+        if (sacarEjecutablesFinalizados().size() != 0) {
+            procesos.addAll(sacarEjecutablesFinalizados());
+        }
+        if (procesos.size() == 0) {
+            System.out.println("No hay procesos finalizados");
+        }
+        return procesos;
+    }
+
+    /**
+     * Metodo que se encarga de retornar los procesos con el estado de "ASIGNADO"
+     * @return procesos - ArrayList de procesos con el estado de "ASIGNADO"
+     */
+    private ArrayList<Proceso> sacarProcesosAsignados(){
+        ArrayList<Proceso> procesos = new ArrayList<>();
+        if (sacarDocumentosAsignados().size() != 0) {
+            procesos.addAll(sacarDocumentosAsignados());
+        }
+        if (sacarMultimediaAsignados().size() != 0) {
+            procesos.addAll(sacarMultimediaAsignados());
+        }
+        if (sacarEjecutablesAsignados().size() != 0) {
+            procesos.addAll(sacarEjecutablesAsignados());
+        }
+        if (procesos.size() == 0) {
+            System.out.println("No hay procesos asignados");
+        }
+        return procesos;
+    }
+
+    /**
+     * Metodo que se encarga de retornar los procesos con el estado que se le pase por parametro
+     * @param estado - Estado de los procesos
+     * @return procesos - ArrayList de procesos con el estado que se le pase por parametro
+     */
+    public ArrayList<Proceso> sacarProcesosPorEstado(String estado){
+        ArrayList<Proceso> procesos = new ArrayList<>();
+        if (estado.equals("ESPERA")) {
+            procesos.addAll(sacarProcesosEnEspera());
+        }
+        else if (estado.equals("FINALIZADO")) {
+            procesos.addAll(sacarProcesosFinalizados());
+        }
+        else if (estado.equals("ASIGNADO")) {
+            procesos.addAll(sacarProcesosAsignados());
+        }
+        else{
+            System.out.println("No hay procesos con ese estado");
+        }
+        return procesos;
+    }
+
+    /**
+     * Metodo que se encarga de imprimir los procesos con el estado ASIGNADO de tipo Documento
+     */
+    private void imprimirDocumentosAsignados(){
+        this.manejo.getLector().forEach((lector) -> {
+            System.out.println("Documentos asignados");
+            System.out.println("Del Usario: " + lector.getUsuario());
+            if (lector.getDocumentos().size() != 0) {
+                lector.getDocumentos().forEach((documento) -> {
+                    if (documento.getEstado().equals("ASIGNADO")) {
+                        System.out.println("Nombre: " + documento.getNombre());
+                        System.out.println("Tamaño: " + documento.getTamano());
+                        System.out.println("Duracion: " + documento.getDuracion());
+                        System.out.println("Tipo: " + documento.getTipo());
+                    }
+                    System.out.println("--------------------------------------------------");
+                });
+            }
+            else{
+                System.out.println("No hay documentos");
+                return;
+            }
+        });
+    }
+
+    /**
+     * Metodo que se encarga de imprimir los procesos con el estado ASIGNADO de tipo Multimedia
+     */
+    private void imprimirMultimediaAsignados(){
+        this.manejo.getLector().forEach((lector) -> {
+            System.out.println("Multimedia asignados");
+            System.out.println("Del Usario: " + lector.getUsuario());
+            if (lector.getMultimedia().size() != 0) {
+                lector.getMultimedia().forEach((multimedia) -> {
+                    if (multimedia.getEstado().equals("ASIGNADO")) {
+                        System.out.println("Nombre: " + multimedia.getNombre());
+                        System.out.println("Tamaño: " + multimedia.getTamano());
+                        System.out.println("Duracion: " + multimedia.getDuracion());
+                        System.out.println("Tipo: " + multimedia.getTipo());
+                    }
+                    System.out.println("--------------------------------------------------");
+                });
+            }
+            else{
+                System.out.println("No hay multimedia");
+                return;
+            }
+        });
+    }
+
+    /**
+     * Metodo que se encarga de imprimir los procesos con el estado ASIGNADO de tipo Ejecutable
+     */
+    private void imprimirEjecutablesAsignados(){
+        this.manejo.getLector().forEach((lector) -> {
+            System.out.println("Ejecutables asignados");
+            System.out.println("Del Usario: " + lector.getUsuario());
+            if (lector.getEjecutables().size() != 0) {
+                lector.getEjecutables().forEach((ejecutable) -> {
+                    if (ejecutable.getEstado().equals("ASIGNADO")) {
+                        System.out.println("Nombre: " + ejecutable.getNombre());
+                        System.out.println("Tamaño: " + ejecutable.getTamano());
+                        System.out.println("Duracion: " + ejecutable.getDuracion());
+                        System.out.println("Tipo: " + ejecutable.getTipo());
+                    }
+                    System.out.println("--------------------------------------------------");
+                });
+            }
+            else{
+                System.out.println("No hay ejecutables");
+                return;
+            }
+        });
+    }
+
+    /**
+     * Metodo que se encarga de imprimir los procesos con el estado ASIGNADO
+     */
+    private void imprimirProcesosAsignados(){
+        if (sacarDocumentosAsignados().size() != 0) {
+            imprimirDocumentosAsignados();
+        }
+        if (sacarMultimediaAsignados().size() != 0) {
+            imprimirMultimediaAsignados();
+        }
+        if (sacarEjecutablesAsignados().size() != 0) {
+            imprimirEjecutablesAsignados();
+        }
+        if (sacarDocumentosAsignados().size() == 0 && sacarMultimediaAsignados().size() == 0 && sacarEjecutablesAsignados().size() == 0) {
+            System.out.println("No hay procesos asignados");
+        }
+    }
+
+    /**
+     * Metodo que se encarga de imprimir los procesos con el estado FINALIZADO de tipo Documento
+     */
+    private void imprimirDocumentosFinalizados(){
+        this.manejo.getLector().forEach((lector) -> {
+            System.out.println("Documentos finalizados");
+            System.out.println("Del Usario: " + lector.getUsuario());
+            if (lector.getDocumentos().size() != 0) {
+                lector.getDocumentos().forEach((documento) -> {
+                    if (documento.getEstado().equals("FINALIZADO")) {
+                        System.out.println("Nombre: " + documento.getNombre());
+                        System.out.println("Tamaño: " + documento.getTamano());
+                        System.out.println("Duracion: " + documento.getDuracion());
+                        System.out.println("Tipo: " + documento.getTipo());
+                    }
+                    System.out.println("--------------------------------------------------");
+                });
+            }
+            else{
+                System.out.println("No hay documentos");
+                return;
+            }
+        });
+    }
+
+    /**
+     * Metodo que se encarga de imprimir los procesos con el estado FINALIZADO de tipo Multimedia
+     */
+    private void imprimirMultimediaFinalizados(){
+        this.manejo.getLector().forEach((lector) -> {
+            System.out.println("Multimedia finalizados");
+            System.out.println("Del Usario: " + lector.getUsuario());
+            if (lector.getMultimedia().size() != 0) {
+                lector.getMultimedia().forEach((multimedia) -> {
+                    if (multimedia.getEstado().equals("FINALIZADO")) {
+                        System.out.println("Nombre: " + multimedia.getNombre());
+                        System.out.println("Tamaño: " + multimedia.getTamano());
+                        System.out.println("Duracion: " + multimedia.getDuracion());
+                        System.out.println("Tipo: " + multimedia.getTipo());
+                    }
+                    System.out.println("--------------------------------------------------");
+                });
+            }
+            else{
+                System.out.println("No hay multimedia");
+                return;
+            }
+        });
+    }
+
+    /**
+     * Metodo que se encarga de imprimir los procesos con el estado FINALIZADO de tipo Ejecutable
+     */
+    private void imprimirEjecutablesFinalizados(){
+        this.manejo.getLector().forEach((lector) -> {
+            System.out.println("Ejecutables finalizados");
+            System.out.println("Del Usario: " + lector.getUsuario());
+            if (lector.getEjecutables().size() != 0) {
+                lector.getEjecutables().forEach((ejecutable) -> {
+                    if (ejecutable.getEstado().equals("FINALIZADO")) {
+                        System.out.println("Nombre: " + ejecutable.getNombre());
+                        System.out.println("Tamaño: " + ejecutable.getTamano());
+                        System.out.println("Duracion: " + ejecutable.getDuracion());
+                        System.out.println("Tipo: " + ejecutable.getTipo());
+                    }
+                    System.out.println("--------------------------------------------------");
+                });
+            }
+            else{
+                System.out.println("No hay ejecutables");
+                return;
+            }
+        });
+    }
+
+    /**
+     * Metodo que se encarga de imprimir los procesos con el estado FINALIZADO
+     */
+    private void imprimirProcesosFinalizados(){
+        if (sacarDocumentosFinalizados().size() != 0) {
+            imprimirDocumentosFinalizados();
+        }
+        if (sacarMultimediaFinalizados().size() != 0) {
+            imprimirMultimediaFinalizados();
+        }
+        if (sacarEjecutablesFinalizados().size() != 0) {
+            imprimirEjecutablesFinalizados();
+        }
+        if (sacarDocumentosFinalizados().size() == 0 && sacarMultimediaFinalizados().size() == 0 && sacarEjecutablesFinalizados().size() == 0) {
+            System.out.println("No hay procesos finalizados");
+        }
+    }
+
+    /**
+     * Metodo que se encarga de imprimir los procesos con el estado ESPERA de tipo Documento
+     */
+    private void imprimirDocumentosEnEspera(){
+        this.manejo.getLector().forEach((lector) -> {
+            System.out.println("Documentos en espera");
+            System.out.println("Del Usario: " + lector.getUsuario());
+            if (lector.getDocumentos().size() != 0) {
+                lector.getDocumentos().forEach((documento) -> {
+                    if (documento.getEstado().equals("ESPERA")) {
+                        System.out.println("Nombre: " + documento.getNombre());
+                        System.out.println("Tamaño: " + documento.getTamano());
+                        System.out.println("Duracion: " + documento.getDuracion());
+                        System.out.println("Tipo: " + documento.getTipo());
+                    }
+                    System.out.println("--------------------------------------------------");
+                });
+            }
+            else{
+                System.out.println("No hay documentos");
+                return;
+            }
+        });
+    }
+
+    /**
+     * Metodo que se encarga de imprimir los procesos con el estado ESPERA de tipo Multimedia
+     */
+    private void imprimirMultimediaEnEspera(){
+        this.manejo.getLector().forEach((lector) -> {
+            System.out.println("Multimedia en espera");
+            System.out.println("Del Usario: " + lector.getUsuario());
+            if (lector.getMultimedia().size() != 0) {
+                lector.getMultimedia().forEach((multimedia) -> {
+                    if (multimedia.getEstado().equals("ESPERA")) {
+                        System.out.println("Nombre: " + multimedia.getNombre());
+                        System.out.println("Tamaño: " + multimedia.getTamano());
+                        System.out.println("Duracion: " + multimedia.getDuracion());
+                        System.out.println("Tipo: " + multimedia.getTipo());
+                    }
+                    System.out.println("--------------------------------------------------");
+                });
+            }
+            else{
+                System.out.println("No hay multimedia");
+                return;
+            }
+        });
+    }
+
+    /**
+     * Metodo que se encarga de imprimir los procesos con el estado ESPERA de tipo Ejecutable
+     */
+    private void imprimirEjecutablesEnEspera(){
+        this.manejo.getLector().forEach((lector) -> {
+            System.out.println("Ejecutables en espera");
+            System.out.println("Del Usario: " + lector.getUsuario());
+            if (lector.getEjecutables().size() != 0) {
+                lector.getEjecutables().forEach((ejecutable) -> {
+                    if (ejecutable.getEstado().equals("ESPERA")) {
+                        System.out.println("Nombre: " + ejecutable.getNombre());
+                        System.out.println("Tamaño: " + ejecutable.getTamano());
+                        System.out.println("Duracion: " + ejecutable.getDuracion());
+                        System.out.println("Tipo: " + ejecutable.getTipo());
+                    }
+                    System.out.println("--------------------------------------------------");
+                });
+            }
+            else{
+                System.out.println("No hay ejecutables");
+                return;
+            }
+        });
+    }
+
+    /**
+     * Metodo que se encarga de imprimir los procesos con el estado ESPERA
+     */
+    private void imprimirProcesosEnEspera(){
+        if (sacarDocumentosEnEspera().size() != 0) {
+            imprimirDocumentosEnEspera();
+        }
+        if (sacarMultimediaEnEspera().size() != 0) {
+            imprimirMultimediaEnEspera();
+        }
+        if (sacarEjecutablesEnEspera().size() != 0) {
+            imprimirEjecutablesEnEspera();
+        }
+        if (sacarDocumentosEnEspera().size() == 0 && sacarMultimediaEnEspera().size() == 0 && sacarEjecutablesEnEspera().size() == 0) {
+            System.out.println("No hay procesos en espera");
+        }
+    }
+
+    /**
+     * Metodo que se encarga de imprimir los procesos con el estado que se le pase por parametro
+     * @param estado - Estado de los procesos
+     */
+    public void imprimirProcesosPorEstado(String estado){
+        if (estado.equals("ESPERA")) {
+            imprimirProcesosEnEspera();
+        }
+        else if (estado.equals("FINALIZADO")) {
+            imprimirProcesosFinalizados();
+        }
+        else if (estado.equals("ASIGNADO")) {
+            imprimirProcesosAsignados();
+        }
+        else{
+            System.out.println("No hay procesos con ese estado");
+        }
+    }
+
+    /**
+     * Metodo que se encarga de imprimir los procesos en orden de estado
+     */
+    public void imprimirProcesos(){
+        System.out.println("Procesos en orden de estado");
+        System.out.println("====================================");
+        System.out.println("Procesos en espera:\n");
+        imprimirProcesosEnEspera();
+        System.out.println("====================================");
+        System.out.println("Procesos asignados:\n");
+        imprimirProcesosAsignados();
+        System.out.println("====================================");
+        System.out.println("Procesos finalizados:\n");
+        imprimirProcesosFinalizados();
+        System.out.println("====================================");
+    }
+
 }
--- JeffryMelanieNorman/src/main/java/ManejoPRS/PRSReader.java
+++ JeffryMelanieNorman/src/main/java/ManejoPRS/PRSReader.java
@@ -9,6 +9,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FilenameFilter;
 import java.io.IOException;
+
+import Componentes.ListaProcesos;
 /**
  * Clase que se encarga de leer los archivos .prs
  *
@@ -29,13 +31,81 @@ public class PRSReader {
      * @param args  - Argumentos de la línea de comandos
      */
     public static void main(String[] args) {
-        ManejoPRS manejo = new ManejoPRS();
-
-        manejo.getLector().forEach((lector) -> {
-            lector.mostrarProcesos();
-            System.out.println("--------------------------------------------------");
-            System.out.println("--------------------------------------------------");
-        });
+        System.out.println("Leyendo archivos .prs");
+        ListaProcesos lista = new ListaProcesos();
+        System.out.println("====================================");
+        System.out.println("Procesos leidos:\n");
+        System.out.println("====================================");
+        System.out.println("Elija el tipo de estado que desea ver (1, 2, 3, 4 o 0)");
+        System.out.println("1. Espera");
+        System.out.println("2. Asignado");
+        System.out.println("3. Finalizado");
+        System.out.println("4. Todos");
+        System.out.println("0. Salir");
+        System.out.println("====================================");
+        System.out.println("Ingrese el numero de la opcion que desea ver");
+        System.out.println("====================================");
+        int opcion = 0;
+        try {
+            BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+            opcion = Integer.parseInt(br.readLine());
+        } catch (IOException e) {
+            System.out.println("Error al leer la opcion");
+        }
+        while (true) {
+            if (opcion == 1) {
+                System.out.println("====================================");
+                System.out.println("Procesos en espera:\n");
+                System.out.println("====================================");
+                lista.imprimirProcesosPorEstado("ESPERA");
+            } else if (opcion == 2) {
+                System.out.println("====================================");
+                System.out.println("Procesos asignados:\n");
+                System.out.println("====================================");
+                lista.imprimirProcesosPorEstado("ASIGNADO");
+            } else if (opcion == 3) {
+                System.out.println("====================================");
+                System.out.println("Procesos finalizados:\n");
+                System.out.println("====================================");
+                lista.imprimirProcesosPorEstado("FINALIZADO");
+            } else if (opcion == 4) {
+                System.out.println("====================================");
+                System.out.println("Procesos:\n");
+                System.out.println("====================================");
+                lista.imprimirProcesos();
+            } else if (opcion == 0) {
+                System.out.println("====================================");
+                System.out.println("Saliendo...");
+                System.out.println("====================================");
+                break;
+            }
+            System.out.println("====================================");
+            System.out.println("Elija el tipo de estado que desea ver (1, 2 o 3)");
+            System.out.println("1. Espera");
+            System.out.println("2. Asignado");
+            System.out.println("3. Finalizado");
+            System.out.println("4. Todos");
+            System.out.println("====================================");
+            System.out.println("Ingrese el numero de la opcion que desea ver");
+            System.out.println("====================================");
+            try {
+                BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+                opcion = Integer.parseInt(br.readLine());
+            } catch (IOException e) {
+                System.out.println("Error al leer la opcion");
+            }
+            while (opcion < 1 || opcion > 4) {
+                System.out.println("====================================");
+                System.out.println("Ingrese una opcion valida");
+                System.out.println("====================================");
+                try {
+                    BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+                    opcion = Integer.parseInt(br.readLine());
+                } catch (IOException e) {
+                    System.out.println("Error al leer la opcion");
+                }
+            }
+        }
     }
 }
 
