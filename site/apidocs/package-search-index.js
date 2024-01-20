b53521adda90c6b5132de6be7e9c22941473aaff
Jeffry Araya Ch <jeffry2811ach@gmail.com>
2024-01-20 10:01:34 -0600

    final

---
+++ site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mycompany.jeffrymelanienorman"},{"l":"Componentes"},{"l":"ManejoPRS"},{"l":"Procesos"},{"l":"ventanas"}];updateSearchResults();
